0d4e1d94ff3d1ed91a71c1a2838dcb767654351e
Jairo Curz <jhon_jairo@live.com>
2018-02-21 22:36:40 -0500

    Add shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import {environment} from '../environments/environment';
 import {AngularFirestoreModule} from 'angularfire2/firestore';
 import {AngularFireModule} from 'angularfire2';
+import {SharedModule} from './shared/shared.module';
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import {AngularFireModule} from 'angularfire2';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule
   ],
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,59 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatSnackBarModule,
+  MatToolbarModule,
+  MatDialogModule,
+  MatOptionModule,
+  MatProgressBarModule,
+  MatSelectModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatProgressSpinnerModule
+} from '@angular/material';
+
+import {
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatOptionModule,
+    MatProgressBarModule,
+    MatSelectModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatProgressSpinnerModule
+  ],
+  providers: []
+})
+export class SharedModule { }
