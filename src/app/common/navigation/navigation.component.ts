40f05f63edcda08a0f8b167923c9c2f467a796c3
Jairo Curz <jhon_jairo@live.com>
2018-03-19 18:34:21 -0500

    Menú base

--- src/app/common/navigation/navigation.component.ts
+++ src/app/common/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../auth/auth.service';
 
 @Component({
   selector: 'app-navigation',
@@ -7,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(
+    public auth: AuthService
+    ){ }
   ngOnInit() {
   }
 
